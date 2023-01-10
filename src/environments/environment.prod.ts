a48d3761d4416677fd4532a443b9e000821c7f66
GLOBALHITSS\Master <joao.junior.5@ebserh.gov.br>
2023-01-10 18:04:59 -0300

    auth firebase

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    production: true,
+    apiKey: "AIzaSyB8IQOce5plqaEr2snu25skfXbbP6dcs_Y",
+    authDomain: "ezoomtest-b49de.firebaseapp.com",
+    projectId: "ezoomtest-b49de",
+    storageBucket: "ezoomtest-b49de.appspot.com",
+    messagingSenderId: "430091720706",
+    appId: "1:430091720706:web:7b66d9d213e03639cd6f1b",
+    measurementId: "G-9YVPNXB1GV"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,30 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+   firebaseConfig: {
+    production: false,
+    apiKey: "AIzaSyB8IQOce5plqaEr2snu25skfXbbP6dcs_Y",
+    authDomain: "ezoomtest-b49de.firebaseapp.com",
+    projectId: "ezoomtest-b49de",
+    storageBucket: "ezoomtest-b49de.appspot.com",
+    messagingSenderId: "430091720706",
+    appId: "1:430091720706:web:7b66d9d213e03639cd6f1b",
+    measurementId: "G-9YVPNXB1GV"
+  }
+};
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
